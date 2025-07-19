8e512fa3cfc70f2191ee1639c76e841fa6599a71
codewithsiddhi-java <pisalsiddhi8@gmail.com>
2025-07-19 19:38:50 +0530

    Create ArrayExample.java
    added example of basic array in java

---
+++ ArrayExample.java
@@ -0,0 +1,10 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[] a= {10, 20, 30, 40, 50};
+
+        System.out.println("Array elements are:");
+        for (int i=0;i<6;i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
