93986b80627dd8c419a5ea040202ea9bed923a46
codewithsiddhi-java <pisalsiddhi8@gmail.com>
2025-08-01 15:43:30 +0530

    Create patternstar.java
    *
    * *
    * * *
    * * * *
    pattern

---
+++ patternstar.java
@@ -0,0 +1,15 @@
+public class patternstar
+{
+public static void main(String[] arg)
+{
+int n=4;
+for(int i=1;i<=n;i++)
+{
+for(int j=1;j<=i;j++)
+{
+System.out.print(" * ");
+}
+System.out.print(" \n ");
+}
+}
+}
