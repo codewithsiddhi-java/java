0652c9dfb9c8dc52f38689f33f8672089c1a9ee3
codewithsiddhi-java <pisalsiddhi8@gmail.com>
2025-08-01 14:02:40 +0530

    Create pattern45.java

---
+++ pattern45.java
@@ -0,0 +1,13 @@
+public class pattern45
+{
+public static void main(String[] arg)
+{
+for(int i=5;i>=1;i--)
+{
+for(int j=i;j<=5;j++)
+{
+System.out.print(j + "     ");
+}
+System.out.print(" \n ");
+}
+}}
